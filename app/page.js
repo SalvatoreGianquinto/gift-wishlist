efc62903208c39e31e0575ec9f3ee5a5a6cfb70a
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-12-30 15:34:17 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center text-center px-4 font-mono">
       <div className="bg-white/10 backdrop-blur-lg border border-white/20 p-8 md:p-12 rounded-4xl shadow-2xl text-white w-full max-w-md md:max-w-lg transition-transform hover:scale-[1.02]">
-        <h1 className="text-4xl md:text-5xl font-extrabold mb-4 tracking-tighter leading-tight">
+        <h1 className="text-4xl md:text-5xl font-extrabold mb-4 tracking-tighter leading-tight ">
           I nostri <br className="md:hidden" /> desideri.
         </h1>
         <p className="text-base md:text-lg text-white/80 font-light tracking-wide">
